4f37032ee75ad4cd3a40053fa2ad034f99c15498
MartaRodriguezOnis <marta.ronis@gmail.com>
2022-11-02 16:42:24 +0100

    feat: add ten buttons and resolve exercise 1c

---
+++ ejercicio1c.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const body = document.querySelector('.js_body');
+
+for (let i = 0; i < 10; i++) {
+  let btns = document.createElement('button');
+  btns.innerHTML = `${i + 1} botón`;
+  document.body.appendChild(btns);
+}
